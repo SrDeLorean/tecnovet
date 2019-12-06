1b007734143a5807f1e4ba90401d940e3534b500
Sebastian Ibarra <xebaelvemgador@gmail.com>
2019-12-05 23:45:10 -0300

    a

--- application/controllers/Administrador.php
+++ application/controllers/Administrador.php
@@ -5,28 +5,28 @@ class Administrador extends CI_Controller {
 
 	public function __construct(){
 		parent::__construct();
-	}
-	public function index(){
+    }
+    
+    public function index(){
 		$this->load->view('templateAdmin/header');
 		$this->load->view('administrador/menuAdministrador');
 		$this->load->view('templateAdmin/footer');
     }
 
-    public function usuarios() {
+    public function usuarios(){
      	$this->load->view('templateAdmin/header');
 		$this->load->view('administrador/usuarios');
 		$this->load->view('templateAdmin/footer');
     }
         
-    public function duenos() {
+    public function duenos(){
         $this->load->view('templateAdmin/header');
         $this->load->view('administrador/duenos');
         $this->load->view('templateAdmin/footer');
 	}
 
 	// Carga de archivos para el menu ADMINISTRADOR
-
-    public function perfiles() {
+    public function perfiles(){
         $this->load->view('templateAdmin/header');
 		$this->load->view('administrador/perfiles');
 		$this->load->view('templateAdmin/footer');
